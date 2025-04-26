7d117a2f058d8c34a608961fce8abb502c0419de
Alan Le Goallec <alanlegoallec1990@gmail.com>
2025-04-26 17:35:53 -0400

    persistent volume

--- init.sql
+++ init.sql
@@ -1,11 +1,9 @@
+-- init.sql
 CREATE TABLE random_numbers (
     id SERIAL PRIMARY KEY,
     value FLOAT NOT NULL
 );
 
-INSERT INTO random_numbers (value) VALUES
-    (3.14),
-    (1.618),
-    (2.718),
-    (0.577),
-    (4.669);
+COPY random_numbers(value)
+FROM '/docker-entrypoint-initdb.d/data/random_numbers.csv'
+DELIMITER ',' CSV HEADER;
