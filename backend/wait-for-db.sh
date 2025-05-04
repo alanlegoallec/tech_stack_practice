e487b3c9df2981a78a55d5c14549383cc509ac43
Alan Le Goallec <alanlegoallec1990@gmail.com>
2025-05-03 23:32:26 -0400

    adding test coverage

--- backend/wait-for-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$DB_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
-  >&2 echo "PostgreSQL is unavailable - sleeping"
-  sleep 1
-done
-
->&2 echo "PostgreSQL is up - executing command"
-exec "$@"
